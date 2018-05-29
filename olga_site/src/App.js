f3c97dbea974d1bbfbc8cd3b4c8ecaeb155ecd96
OlgaJanicka <35032091+OlgaJanicka@users.noreply.github.com>
2018-05-29 21:58:11 +0200

    refactoring list component

--- olga_site/src/App.js
+++ olga_site/src/App.js
@@ -8,65 +8,8 @@ class App extends Component {
     super(props);
 
     this.state = {
-      elements: [1, 2, 3, 4, 5],
-      elements2:['a','b','c','d']
+      elements: [1, 2, 3, 4, 5]
     };
-
-    //bind pozwala przypiąć funkcje do .this
-    this.updateInput = this.updateInput.bind(this);
-    this.addListItem = this.addListItem.bind(this);
-  }
-
-  //konwencja
-  updateInput(event) {
-    let validationError = this.getValidationError(event.target.value);
-
-    if (validationError) {
-      this.setState({
-        ...this.state,
-        validationError: validationError,
-        elementValue: event.target.value
-      })
-    } else {
-      this.setState({
-        ...this.state,
-        validationError: "",
-        elementValue: event.target.value
-      })
-    }
-  }
-
-  addListItem(event) {
-    let newElement = this.state.elementValue;
-    let validationError = this.getValidationError(newElement);
-
-    if (validationError) {
-      this.setState({
-        ...this.state,
-        validationError: validationError
-      })
-    } else {
-      let elements = this.state.elements;
-      elements.push(newElement);
-
-      this.setState({
-        ...this.state,
-        elements: elements,
-        elementValue: "",
-      })
-    }
-  }
-
-  getValidationError(value) {
-    if (!value) {
-      return "empty Value not allowed";
-    }else if(value.includes("dupa")){
-      return "dupa jest klasyką js";
-    }else if (value.length > 5 ){
-      return " ho ho za duzo";
-    }
-
-
   }
 
   render() {
@@ -74,30 +17,12 @@ class App extends Component {
       <div className="App">
         <div className="row">
           <div className="col-md-6 mx-auto">
-            {//nasze twory
-            }
-            <OlgaList source={this.state.elements} sortDirection={true}/>
+            
+            <OlgaList source={this.state.elements} sortDirection={true} />
             <br />
-            <OlgaList source={this.state.elements2} sortDirection={true}/>
-            <br />
-            <input className="form-control" value={this.state.elementValue} type="text" onChange={this.updateInput} />
-            {this.state.validationError &&
-              <div style={{ color: "red" }} className="float-left">
-                *{this.state.validationError}
-              </div>
-            }
-
-            {
-              //predefinowane
-            }
-      
           </div>
         </div>
-        <div className="row">
-          <div className="col-md-6 mx-auto">
-          <button className="btn btn-primary" type="button" onClick={this.addListItem}>dodaj</button>
-          </div >
-        </div >
+        
       </div >
     );
   }
--- olga_site/src/OlgaList.jsx
+++ olga_site/src/OlgaList.jsx
@@ -4,22 +4,92 @@ import OlgaListItemPretty from './OlgaListItemPretty';
 
 class OlgaList extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            elements: props.source,
+            sortDirection: false
+        };
+
+        //bind pozwala przypiąć funkcje do .this
+        this.updateInput = this.updateInput.bind(this);
+        this.sortList = this.sortList.bind(this);
+        this.addListItem = this.addListItem.bind(this);
+    }
+
+    getValidationError(value) {
+        if (!value) {
+            return "empty Value not allowed";
+        } else if (value.includes("dupa")) {
+            return "dupa jest klasyką js";
+        } else if (value.length > 5) {
+            return " ho ho za duzo";
+        }
+    }
+
+    addListItem(event) {
+        let newElement = this.state.elementValue;
+        let validationError = this.getValidationError(newElement);
+
+        if (validationError) {
+            this.setState({
+                ...this.state,
+                validationError: validationError
+            })
+        } else {
+            let elements = this.state.elements;
+            elements.push(newElement);
+
+            this.setState({
+                ...this.state,
+                elements: elements,
+                elementValue: "",
+            })
+        }
+    }
+
+    updateInput(event) {
+        let validationError = this.getValidationError(event.target.value);
+
+        if (validationError) {
+            this.setState({
+                ...this.state,
+                validationError: validationError,
+                elementValue: event.target.value
+            })
+        } else {
+            this.setState({
+                ...this.state,
+                validationError: "",
+                elementValue: event.target.value
+            })
+        }
+    }
+
+
+    sortList(){
+        this.setState({
+         ...this.state,
+         sortDirection: !this.state.sortDirection 
+        })
+    }
+
 
     render() {
-
         return (
             <div class="list-group">
-                <button type="button" class="list-group-item list-group-item-action active">
+                <button type="button" class="list-group-item list-group-item-action active" onClick={this.sortList}>
                     Liczby
                      {(() => {
-                        if (this.props.sortDirection) {
+                        if (this.state.sortDirection) {
                             return <i className="fas fa-arrow-circle-down" />
                         } else {
                             return <i className="fas fa-arrow-circle-up" />
                         }
                     })()}
                 </button>
-                {this.props.source.map((element, index) => {
+                {this.state.elements.map((element, index) => {
                     if (element > 4) {
                         return <OlgaListItemPretty content={element} />;
                     } else {
@@ -27,6 +97,18 @@ class OlgaList extends Component {
                     }
                 })
                 }
+                <br />
+                <input className="form-control" value={this.state.elementValue} type="text" onChange={this.updateInput} />
+                {this.state.validationError &&
+                    <div style={{ color: "red" }} className="float-left">
+                        *{this.state.validationError}
+                    </div>
+                }
+                <div className="row">
+                    <div className="col-md-6 mx-auto">
+                        <button className="btn btn-primary" type="button" onClick={this.addListItem}>dodaj</button>
+                    </div >
+                </div >
             </div>
         );
     }
